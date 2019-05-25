4b36057e1ef71f5140aaf18bbbcf35a35ed91198
Arivuchchelvi <42860111+Arivu29@users.noreply.github.com>
2019-05-25 22:52:59 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,21 @@
+package com.vijani.springbootrestapiexample.model;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name="User")
+@EntityListeners(AuditingEntityLis)
+public class User {
+
+}
